f5844be096555221d31c4f687ddec777882d3148
John_Lamy <johnclamy@gmail.com>
2023-06-05 12:28:04 +0100

    add content to the home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,47 @@
-export default function Home() {
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Home({ categories }) {
   return (
-    <p>testing the home page component...</p>
-  )
+    <main>
+      <header>
+        <div>
+          <h1>
+            Welcome to Tech Events. Here you will find the best{" "}
+            <em>Web Development</em> events.
+          </h1>
+          <p>
+            We have a list of all the major web tech event you can find in
+            Europe. Book with us at <strong>no extra cost</strong>.
+          </p>
+          <p>
+            Think <b>Tech Events</b>
+          </p>
+        </div>
+        <Image src="/imgs/hero.png" alt="events" width={145} height={145} />
+      </header>
+      <hr />
+      <section>
+        {categories.map(({ id, title, description, image }) => (
+          <Link key={id} href={`/events/${id}`}>
+            <a>
+              <Image src={image} alt={title} width={640} height={426} />
+              <div>
+                <h2>{title}</h2>
+                <p>{description}</p>
+              </div>
+            </a>
+          </Link>
+        ))}
+      </section>
+    </main>
+  );
+}
+
+export async function getStaticProps() {
+  const { events_categories } = await import('../data/index.json')
+
+  return {
+    props: { categories: events_categories },
+  };
 }

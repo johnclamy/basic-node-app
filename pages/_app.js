27e2480b6d1813df8a1128a7cbac2c9f1a5294a0
John_Lamy <johnclamy@gmail.com>
2023-06-07 10:43:06 +0100

    define the app layoutand styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,10 @@
+import Layout from "../src/components/Layout";
+import "../styles/global.css";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }

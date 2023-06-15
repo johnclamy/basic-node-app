43aa49701036177ed497ad32f50b550a89862977
John_Lamy <johnclamy@gmail.com>
2023-06-15 21:17:39 +0100

    move the links in Navbar to their own component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,18 @@
-import { useState } from "react";
-import Link from 'next/link';
-import styles from './Navbar.module.css';
-import utils from '../../styles/utils.module.css';
+import { GiHamburgerMenu } from "react-icons/gi"
+import Logo from "./Logo"
+import NavLinks from './NavLinks'
+import { useWindowSize } from "../hooks"
+import styles from './Navbar.module.css'
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false)
+  const size = useWindowSize()
 
   return (
-    <section className={styles["navbar-wrapper"]}>
-      <div className={styles.navbar}>
-        <Link href="/">
-          <a className={utils.logo}>🎟️ tech events</a>
-        </Link>
-        <nav className={styles["nav-links"]}>
-          <Link href="/">
-            <a className={styles["nav-link"]}>home</a>
-          </Link>
-          <Link href="/events">
-            <a className={styles["nav-link"]}>events</a>
-          </Link>
-          <Link href="/about">
-            <a className={styles["nav-link"]}>about</a>
-          </Link>
-        </nav>
-        <button className={styles.hamburger}>
-          <span className={styles.bar}></span>
-          <span className={styles.bar}></span>
-          <span className={styles.bar}></span>
-        </button>
-      </div>
+    <section className={styles.navbar}>
+      <Logo />
+      {size.width > 768 ?
+        <NavLinks /> :
+        <GiHamburgerMenu size={30} className={styles.hamburger} />}
     </section>
-  );
+  )
 }

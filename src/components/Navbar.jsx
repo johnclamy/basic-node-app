fef6d55e69a34942bf81e14c6e2949a1d88a14e7
John_Lamy <johnclamy@gmail.com>
2023-06-16 14:51:30 +0100

    set the hamburger icon for the drawer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,24 @@
+import { useState } from "react"
 import { GiHamburgerMenu } from "react-icons/gi"
 import Logo from "./Logo"
 import NavLinks from './NavLinks'
+import Drawer from "./Drawer"
 import { useWindowSize } from "../hooks"
 import styles from './Navbar.module.css'
 
 export default function Navbar() {
   const size = useWindowSize()
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false)
+  const openDrawer = () => setDrawerIsOpen(true)
+  const closeDrawer = () => setDrawerIsOpen(false)
 
   return (
     <section className={styles.navbar}>
       <Logo />
       {size.width > 768 ?
         <NavLinks /> :
-        <GiHamburgerMenu size={30} className={styles.hamburger} />}
+        <GiHamburgerMenu onClick={openDrawer} size={30} className={styles.hamburger} />}
+      {drawerIsOpen && <Drawer onCloseDrawer={closeDrawer} />}
     </section>
   )
 }

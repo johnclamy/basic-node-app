8219bcb394b6fd1103e189fee2b478caea25476a
John_Lamy <johnclamy@gmail.com>
2023-06-16 17:12:48 +0100

    remove duplicate main tag

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Head from "next/head";
 import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home({ categories }) {
   return (
@@ -8,38 +9,38 @@ export default function Home({ categories }) {
       <Head>
         <title>Welcome to Tech Events | Find the best Web Dev events</title>
       </Head>
-      <main>
-        <header>
-          <div>
-            <h1>
-              Welcome to Tech Events. Here you will find the best{" "}
-              <em>Web Development</em> events.
-            </h1>
-            <p>
-              We have a list of all the major web tech event you can find in
-              Europe. Book with us at <strong>no extra cost</strong>.
-            </p>
-            <p>
-              Think <b>Tech Events</b>
-            </p>
-          </div>
-          <Image src="/imgs/hero.png" alt="events" width={145} height={145} />
-        </header>
-        <hr />
-        <section>
-          {categories.map(({ id, title, description, image }) => (
-            <Link key={id} href={`/events/${id}`}>
-              <a>
-                <Image src={image} alt={title} width={640} height={426} />
-                <div>
-                  <h2>{title}</h2>
-                  <p>{description}</p>
-                </div>
-              </a>
-            </Link>
-          ))}
-        </section>
-      </main>
+      <header>
+        <div>
+          <h1 className={styles.title}>
+            Welcome to Tech Events. Here you will find the best{" "}
+            <em>Web Development</em> events.
+          </h1>
+          <p className={styles.subtitle}>
+            We have a list of all the major web tech event you can find in
+            Europe. Book with us at <strong>no extra cost</strong>.
+          </p>
+          <p>
+            Think <b>Tech Events</b> 👍
+          </p>
+        </div>
+        <figure className={styles["events-img"]}>
+          <Image src="/imgs/hero.png" alt="events" width={165} height={165} />
+        </figure>
+      </header>
+      <hr />
+      <section>
+        {categories.map(({ id, title, description, image }) => (
+          <Link key={id} href={`/events/${id}`}>
+            <a>
+              <Image src={image} alt={title} width={640} height={426} />
+              <div className={styles['category-info']}>
+                <h2>{title}</h2>
+                <p>{description}</p>
+              </div>
+            </a>
+          </Link>
+        ))}
+      </section>
     </>
   );
 }

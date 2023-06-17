0d7d5835210bdc94587d43b3caf519c8a904b26f
John_Lamy <johnclamy@gmail.com>
2023-06-17 19:22:07 +0100

    style the 'about us' page

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,8 +1,9 @@
 import Image from 'next/image'
+import styles from '../styles/About-us.module.css'
 
 export default function About() {
   return (
-    <main>
+    <>
       <Image
         src="/imgs/about.jpg"
         alt="office workers in a boardroom meeting"
@@ -10,23 +11,23 @@ export default function About() {
         width="640"
         height="427"
       />
-      <section>
-        <h1>
+      <section className={styles.container}>
+        <h1 className={styles.title}>
           Who we are and how we got here
         </h1>
-        <p>
+        <p className={styles.text}>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
           omnis voluptatem accusantium nemo perspiciatis delectus atque
           autem! Voluptatum tenetur beatae unde aperiam, repellat expedita
           consequatur! Officiis id consequatur atque doloremque!
         </p>
-        <p>
+        <p className={styles.text}>
           {" "}
           Nobis minus voluptatibus pariatur dignissimos libero quaerat
           iure expedita at? Asperiores nemo possimus nesciunt dicta veniam
           aspernatur quam mollitia.
         </p>
       </section>
-    </main>
+    </>
   )
 }

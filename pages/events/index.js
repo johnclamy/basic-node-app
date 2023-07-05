e1ea98bd77e4a84ab508eceb9424125470ad8c99
John_Lamy <johnclamy@gmail.com>
2023-07-05 12:40:52 +0100

    use useState hook on initial events data

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../../styles/Events.module.css";
 
-const events = [
+const eventsData = [
   {
     imgFileName: "london-events.png",
     location: "London",
@@ -21,34 +22,37 @@ const events = [
 ];
 
 export default function Events() {
+  const [events, setEvents] = useState(eventsData)
   return (
-    <>
+    <div>
       <header>
         <h1 className={styles.title}>events</h1>
       </header>
-      <ul>
-        {events.map(({ imgFileName, location, slogan }) => (
-          <li key={location} className={styles["category-item"]}>
-            <Link href={`/events/${location.toLowerCase()}`} passHref>
-              <a>
-                <figure>
-                  <Image
-                    width={150}
-                    height={150}
-                    alt={`Events in ${location}`}
-                    src={`/imgs/${imgFileName}`}
-                    className={styles.image}
-                  />
-                  <figcaption>
-                    <h3 className={styles["category-title"]}>{location}</h3>
-                    <p className={styles.text}>{slogan}</p>
-                  </figcaption>
-                </figure>
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </>
+      <div className={styles.wrapper}>
+        <ul className={styles.container}>
+          {events.map(({ imgFileName, location, slogan }) => (
+            <li key={location} className={styles.event}>
+              <Link href={`/events/${location.toLowerCase()}`} passHref>
+                <a>
+                  <figure className={styles.figure}>
+                    <Image
+                      width={300}
+                      height={300}
+                      alt={`Events in ${location}`}
+                      src={`/imgs/${imgFileName}`}
+                      className={styles.image}
+                    />
+                    <figcaption>
+                      <h3 className={styles["category-title"]}>{location}</h3>
+                      <p className={styles.text}>{slogan}</p>
+                    </figcaption>
+                  </figure>
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }

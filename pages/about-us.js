bf69828ddc7f54519451f5f2238bc492794b9c7e
John_Lamy <johnclamy@gmail.com>
2023-07-05 01:18:47 +0100

    set media queries for larger screens

--- pages/about-us.js
+++ pages/about-us.js
@@ -3,31 +3,31 @@ import styles from '../styles/About-us.module.css'
 
 export default function About() {
   return (
-    <>
-      <Image
-        src="/imgs/about.jpg"
-        alt="office workers in a boardroom meeting"
-        loading="lazy"
-        width="640"
-        height="427"
-      />
+    <div className={styles.wrapper}>
+      <div className={styles['image-container']}>
+        <Image
+          src="/imgs/about.jpg"
+          alt="office workers in a boardroom meeting"
+          loading="lazy"
+          width="640"
+          height="427"
+        />
+      </div>
       <section className={styles.container}>
-        <h1 className={styles.title}>
-          Who we are and how we got here
-        </h1>
+        <h1 className={styles.title}>Who we are and how we got here</h1>
         <p className={styles.text}>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
-          omnis voluptatem accusantium nemo perspiciatis delectus atque
-          autem! Voluptatum tenetur beatae unde aperiam, repellat expedita
-          consequatur! Officiis id consequatur atque doloremque!
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis
+          voluptatem accusantium nemo perspiciatis delectus atque autem!
+          Voluptatum tenetur beatae unde aperiam, repellat expedita consequatur!
+          Officiis id consequatur atque doloremque!
         </p>
         <p className={styles.text}>
           {" "}
-          Nobis minus voluptatibus pariatur dignissimos libero quaerat
-          iure expedita at? Asperiores nemo possimus nesciunt dicta veniam
-          aspernatur quam mollitia.
+          Nobis minus voluptatibus pariatur dignissimos libero quaerat iure
+          expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur
+          quam mollitia.
         </p>
       </section>
-    </>
-  )
+    </div>
+  );
 }

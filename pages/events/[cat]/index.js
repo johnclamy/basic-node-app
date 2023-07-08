cdd06f373be522a267d0e19490ccfd307cb2e832
John_Lamy <johnclamy@gmail.com>
2023-07-08 10:34:36 +0100

    render the list of events for a given category

--- pages/events/[cat]/index.js
+++ pages/events/[cat]/index.js
@@ -5,33 +5,35 @@ import styles from '../../../styles/CityEvents.module.css'
 
 export default function EventCategory({ data, city }) {
   return (
-    <article className={styles["article-container"]}>
-      <header>
-        <h1 className={styles.title}>
-          Events in <b className={styles.city}>{city}</b>
-        </h1>
-      </header>
-      <section className={styles.article}>
-        {data.map((event) => (
-          <Link
-            key={event.id}
-            href={`/events/${event.city}/${event.id}`}
-            passHref
-          >
-            <a>
-              <Image
-                width={300}
-                height={300}
-                alt={event.title}
-                src={event.image}
-              />
-              <h2 className={styles["city-title"]}>{event.title}</h2>
-              <p className={styles.text}>{event.description}</p>
-            </a>
-          </Link>
-        ))}
-      </section>
-    </article>
+    <div className={styles.wrapper}>
+      <article className={styles["article-container"]}>
+        <header>
+          <h1 className={styles.title}>
+            Events in <b className={styles.city}>{city}</b>
+          </h1>
+        </header>
+        <section className={styles.article}>
+          {data.map((event) => (
+            <Link
+              key={event.id}
+              href={`/events/${event.city}/${event.id}`}
+              passHref
+            >
+              <a>
+                <Image
+                  width={300}
+                  height={300}
+                  alt={event.title}
+                  src={event.image}
+                />
+                <h2 className={styles["city-title"]}>{event.title}</h2>
+                <p className={styles.text}>{event.description}</p>
+              </a>
+            </Link>
+          ))}
+        </section>
+      </article>
+    </div>
   );
 }
 
